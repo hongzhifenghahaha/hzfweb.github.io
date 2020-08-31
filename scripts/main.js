1618f334d3afd2ea158937a4d5f3e5ad4432a249
Hong Zhifeng <920827856@qq.com>
2020-08-31 10:11:39 +0800

    initial repository

---
+++ scripts/main.js
@@ -0,0 +1,42 @@
+/*
+ * @Author: your name
+ * @Date: 2020-08-31 00:10:52
+ * @LastEditTime: 2020-08-31 00:32:53
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /test-site1/scripts/main.js
+ */
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/firefox-icon.png') {
+      myImage.setAttribute('src', 'images/firefox2.png');
+    } else {
+      myImage.setAttribute('src', 'images/firefox-icon.png');
+    }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt('请输入你的名字。');
+    if(!myName || myName === null) {
+      setUserName();
+    } else {
+      localStorage.setItem('name', myName);
+      myHeading.innerHTML = 'Mozilla 酷毙了，' + myName;
+    }
+  }
+
+  if(!localStorage.getItem('name')) {
+    setUserName();
+  } else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla 酷毙了，' + storedName;
+  }
+
+  myButton.onclick = function() {
+    setUserName();
+ }
